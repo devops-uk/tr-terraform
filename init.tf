728d15db98eb3facc5f8ae545603fdc84fc6365d
thirupathi-p <thirupathiperaboina@gmail.com>
2019-06-14 17:08:10 +0530

    do it

--- init.tf
+++ init.tf
@@ -1,38 +1,57 @@
-variable "cluster_size" {
-  type        = "string"
-  description = "Number of instances in the mongo cluster"
-}
-
-variable "environment" {
-  type        = "string"
-  description = "Environment/production tier"
-}
-
-variable "private_subnets" {
-  type        = "list"
-  description = "List of private subnet IDs Mongo launches in"
-}
-
-variable "region" {
-  type        = "string"
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
-variable "vpc_id" {
-  type        = "string"
-  description = "VPC ID of something we connect to somewhere"
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 provider "aws" {
-  version = "~> 1.2"
-  region  = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-2"
 }
 
-provider "random" {
-  version = "~> 1.0"
+resource "aws_key_pair" "mongodb" {
+  key_name   = "mongodb-key"
+  public_key = "${file("${path.module}/keys/mongodb.pub")}"
 }
 
-provider "template" {
-  version = "~> 1.0"
+resource "aws_security_group" "mongodb" {
+  name        = "mongodb"
+  description = "Allow all inbound SSH and MongoDB traffic."
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    description = "MongoDB access"
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "MongoDB"
+  }
+}
+
+module "mongodb" {
+  source = "../../"
+
+  aws_region        = "us-east-2"
+  availability_zone = "us-east-2a"
+  instance_type     = "t2.micro"
+  volume_size       = "10"
+  key_name          = "${aws_key_pair.mongodb.key_name}"
+  private_key       = "${file("${path.module}/keys/mongodb")}"
+  security_groups   = ["${aws_security_group.mongodb.name}"]
 }
