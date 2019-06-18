27d3469935986c4211cae0fdec12bd2f0c4d54d0
thirupathi-p <thirupathiperaboina@gmail.com>
2019-06-18 10:38:27 +0530

    without creaditials

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-variable "AWS_REGION" {
-  default = "us-east-1"
-}
+# variable "AWS_REGION" {
+#   default = "us-east-1"
+# }
 
 provider "aws" {
-    access_key = "$AWS_ACCESS_KEY_ID"
-    secret_key = "$AWS_SECRET_ACCESS_KEY"    
-   region = "${var.AWS_REGION}"
+  #   access_key = "$AWS_ACCESS_KEY_ID"
+  #   secret_key = "$AWS_SECRET_ACCESS_KEY"    
+  #  region = "${var.AWS_REGION}"
 } 
 
 
