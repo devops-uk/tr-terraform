e0ccdbe59473045f203930f04b6daa22e3d8d31c
thirupathi-p <thirupathiperaboina@gmail.com>
2019-06-18 09:54:21 +0530

    check that

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
+variable "AWS_ACCESS_KEY_ID" {}
 
-# variable "AWS_REGION" {
-#   default = "us-east-1"
-# }
+variable "AWS_SECRET_ACCESS_KEY" {}
 
-provider "aws" {}
-#     access_key = "$AWS_ACCESS_KEY_ID"
-#     secret_key = "$AWS_SECRET_ACCESS_KEY"
-#     region     = "us-east-1"
-    
-#    region = "${var.AWS_REGION}"
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+    access_key = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"    
+   region = "${var.AWS_REGION}"
+} 
 
 
 resource "aws_instance" "example" {
