aa2a1e2cd953253ddcee826c5f1715362e8d7512
thirupathi-p <thirupathi8p@yahoo.com>
2019-07-09 16:04:18 +0530

    yes

--- .circleci/terraform/vars.tf
+++ .circleci/terraform/vars.tf
@@ -1,3 +1,12 @@
+variable "AWS_ACCESS_KEY" {
+  default = "${AWS_ACCESS_KEY_ID}"
+}
+variable "AWS_SECRET_ACCESS" {
+  default = "${AWS_SECRET_ACCESS_KEY}"
+}
+
+
+
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
