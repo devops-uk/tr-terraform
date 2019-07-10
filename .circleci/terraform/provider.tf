10623ca4e8b2193abfe22c42b2da7b0949868ea3
thirupathi-p <thirupathi8p@yahoo.com>
2019-07-10 10:17:07 +0530

    test

--- .circleci/terraform/provider.tf
+++ .circleci/terraform/provider.tf
@@ -1,5 +1,24 @@
 
-
 provider "aws" {   
-    region = "${var.AWS_REGION}"
+    region = "us-east-1"
 }
+resource "aws_vpc" "taskuk" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "taskuk"
+    Location = "Banglore"
+  }
+}
+
+resource "aws_subnet" "taskuk" {
+  vpc_id     = "${aws_vpc.taskuk.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+
--- .circleci/terraform/test.sh
+++
@@ -1,6 +0,0 @@
-key = $AWS_ACCESS_KEY_ID
-secret = $AWS_SECRET_ACCESS_KEY
-
-terraform init
-terraform plan
-terraform apply -auto-approve
