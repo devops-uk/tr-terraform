89a1fba566bbe8d02a35557c37b61b5778563e87
thirupathi-p <thirupathi8p@yahoo.com>
2019-07-11 17:56:33 +0530

    where

--- .circleci/terraform/vpc.tf
+++ .circleci/terraform/vpc.tf
@@ -5,9 +5,9 @@ resource "aws_vpc" "default" {
   cidr_block = "${var.vpc_cidr}"
   enable_dns_hostnames = true
 
-#   tags {
-#     Name = "test-vpc"
-#   }
+  tags {
+    Name = "test-vpc"
+  }
 }
 
 # Define the public subnet
