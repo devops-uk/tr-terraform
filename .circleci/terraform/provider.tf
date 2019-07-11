1f806a48c6bf968bd7f259947bfbd467f709008a
thirupathi-p <thirupathi8p@yahoo.com>
2019-07-11 17:51:33 +0530

    test

--- .circleci/terraform/provider.tf
+++ .circleci/terraform/provider.tf
@@ -1,24 +1,4 @@
-
-provider "aws" {   
-    region = "us-east-1"
+# Define AWS as our provider
+provider "aws" {
+  region = "${var.aws_region}"
 }
-resource "aws_vpc" "taskuk" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "taskuk"
-    Location = "Banglore"
-  }
-}
-
-resource "aws_subnet" "taskuk" {
-  vpc_id     = "${aws_vpc.taskuk.id}"
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "subnet1"
-  }
-}
-
-
