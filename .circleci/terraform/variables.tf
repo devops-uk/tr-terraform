0bbcb3345b2e9f805268a8eadc6d6cfeb7e47305
thirupathi-p <thirupathi8p@yahoo.com>
2019-07-12 10:06:09 +0530

    go than

--- .circleci/terraform/variables.tf
+++ .circleci/terraform/variables.tf
@@ -7,17 +7,17 @@ variable "aws_region" {
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
-  default = "10.300.0.0/16"
+  default = "10.200.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
-  default = "10.300.1.0/24"
+  default = "10.200.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR for the private subnet"
-  default = "10.300.2.0/24"
+  default = "10.200.2.0/24"
 }
 
 variable "ami" {
