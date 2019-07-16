5294d618dffb58d0c33ed7bca0a5ff62157209fa
thirupathi-p <thirupathi8p@yahoo.com>
2019-07-16 14:32:14 +0530

    more tht

--- environments/production/resources.tf
+++ environments/production/resources.tf
@@ -25,11 +25,11 @@
 resource "aws_instance" "wb" {
    ami  = "${var.ami}"
    instance_type = "t1.micro"
-   subnet_id = "${aws_subnet.public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
-   associate_public_ip_address = true
-   source_dest_check = false
-   user_data = "${file("userdata.sh")}"
+  #  subnet_id = "${aws_subnet.public-subnet.id}"
+  #  vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+  #  associate_public_ip_address = true
+  #  source_dest_check = false
+  #  user_data = "${file("userdata.sh")}"
 
   tags = {
     Name = "webserver"
@@ -39,10 +39,10 @@ resource "aws_instance" "wb" {
 # Define database inside the private subnet
 resource "aws_instance" "db" {
    ami  = "${var.ami}"
-   instance_type = "t1.micro"
-   subnet_id = "${aws_subnet.private-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.sgdb.id}"]
-   source_dest_check = false
+  #  instance_type = "t1.micro"
+  #  subnet_id = "${aws_subnet.private-subnet.id}"
+  #  vpc_security_group_ids = ["${aws_security_group.sgdb.id}"]
+  #  source_dest_check = false
 
   tags = {
     Name = "database"
